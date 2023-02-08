3063f6203396c7671b1b58272389a60d7fbe9f62
sahil0209 <sahilharjai2001@gmail.com>
2023-02-08 09:42:06 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { LmoviesComponent } from './lmovies/lmovies.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LmoviesComponent,
+    NewsletterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/lmovies/lmovies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lmovies',
+  templateUrl: './lmovies.component.html',
+  styleUrls: ['./lmovies.component.css']
+})
+export class LmoviesComponent {
+
+}
